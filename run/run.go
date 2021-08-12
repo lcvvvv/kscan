561bc1b9d13438249e330d126510982127e9439f
kv2 <cheng.kv2@gmail.com>
2021-08-12 20:43:17 +0800

    [*]修复在check模式下存在的一些Bug

--- run/run.go
+++ run/run.go
@@ -14,36 +14,34 @@ func Start(config app.Config) {
 	go K.WatchDog()
 
 	if config.Check {
-		slog.Info("当前为验证模式，不会进行端口扫描，仅对给定URL地址进行指纹识别")
+		slog.Info("当前为验证模式，不会进行端口扫描，仅对给定地址进行指纹识别")
+		//STEP1:验证模式直接进行应用层指纹识别
 		time.Sleep(time.Microsecond * 200)
 		go K.GetAppBannerFromCheck()
-		////STEP4: 输出
-		K.Output()
-		return
+	} else {
+		//STEP1:主机存活性检测
+		time.Sleep(time.Microsecond * 200)
+		go K.HostDiscovery(K.config.HostTarget, config.ScanPing)
+
+		//STEP2：端口存活性检测
+		time.Sleep(time.Microsecond * 200)
+		go K.PortDiscovery()
+
+		//STEP3：TCP指纹识别
+		time.Sleep(time.Microsecond * 200)
+		go K.GetTcpBanner()
+
+		//STEP3: 应用层指纹识别
+		time.Sleep(time.Microsecond * 200)
+		go K.GetAppBanner()
 	}
 
-	//STEP1:主机存活性检测
-	time.Sleep(time.Microsecond * 200)
-	go K.HostDiscovery(K.config.HostTarget, config.ScanPing)
-
-	//STEP2：端口存活性检测
-	time.Sleep(time.Microsecond * 200)
-	go K.PortDiscovery()
-
-	//STEP3：TCP指纹识别
-	time.Sleep(time.Microsecond * 200)
-	go K.GetTcpBanner()
-
-	//STEP3: 应用层指纹识别
-	time.Sleep(time.Microsecond * 200)
-	go K.GetAppBanner()
-
-	//STEP4: 暴力破解
+	//STEP_A: 暴力破解
 	time.Sleep(time.Microsecond * 200)
 	if app.Setting.Hydra {
 		time.Sleep(time.Microsecond * 200)
 		go K.Hydra()
 	}
-	////STEP4: 输出
+	//STEP_B: 输出
 	K.Output()
 }
