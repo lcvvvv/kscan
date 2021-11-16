b3f22d5d86bc9885cf40f0aee364c7e18473b42b
kv2 <cheng.kv2@gmail.com>
2021-11-16 20:08:46 +0800

    [+]使用更新的rdp暴力破解模块，丢弃freerdp~

--- lib/hydra/rdp/rdp_test.go
+++ lib/hydra/rdp/rdp_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestRdp(t *testing.T) {
-	r, err := Check("192.168.217.233", "workgroup", "Administrator", "zaq1@WSX", 3389)
+	r, err := Check("192.168.217.147", "", "Administrator", "zaq1@WSX", 3389)
 	if err != nil {
 		fmt.Println(err)
 	}
