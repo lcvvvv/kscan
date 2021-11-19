991e1d075367bbcddeceabaa38d98fd9c8645743
kv2 <cheng.kv2@gmail.com>
2021-11-19 19:44:44 +0800

    [*]关闭mssql在进行测试时的错误日志

--- lib/hydra/mssql/mssql.go
+++ lib/hydra/mssql/mssql.go
@@ -4,10 +4,14 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/denisenkom/go-mssqldb"
+	mssql "github.com/denisenkom/go-mssqldb"
+	"io"
+	"log"
 	"time"
 )
 
 func Check(Host, Username, Password string, Port int) (bool, error) {
+	mssql.SetLogger(log.New(io.Discard, "", log.Ldate|log.Ltime))
 	dataSourceName := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%v;encrypt=disable;timeout=%v", Host, Username, Password, Port, 5*time.Second)
 	db, err := sql.Open("mssql", dataSourceName)
 	if err != nil {
