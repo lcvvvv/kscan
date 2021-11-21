52e6d64f52f24178ca0faf7c4a2d036f933a649f
kv2 <cheng.kv2@gmail.com>
2021-11-21 16:31:36 +0800

    [*]重新支持oracle暴力破解

--- lib/hydra/oracle/oracle.go
+++ lib/hydra/oracle/oracle.go
@@ -1,21 +1,37 @@
 package oracle
 
 import (
-	_ "github.com/godror/godror"
+	"database/sql"
+	"fmt"
+	_ "github.com/sijms/go-ora/v2"
+	"time"
 )
 
-//func Check(Host, Username, Password string, Port int) (bool, error) {
-//	dataSourceName := fmt.Sprintf(`user="%v" password="%v" connectString="%v:%d/orcl?connect_timeout=%v"`, Username, Password, Host, Port, time.Second*5)
-//	db, err := sql.Open("godror", dataSourceName)
-//	if err != nil {
-//		return false, err
-//	}
-//	db.SetConnMaxLifetime(5 * time.Second)
-//	db.SetMaxIdleConns(0)
-//	defer db.Close()
-//	err = db.Ping()
-//	if err != nil {
-//		return false, err
-//	}
-//	return true, nil
-//}
+var ServiceName = []string{
+	"orcl",
+	"xe",
+	"oracle",
+}
+
+func Check(Host, Username, Password string, Port int) (bool, error) {
+	var db *sql.DB
+	var err error
+	for _, service := range ServiceName {
+		dataSourceName := fmt.Sprintf("oracle://%s:%s@%s:%d/%s", Username, Password, Host, Port, service)
+		db, err = sql.Open("oracle", dataSourceName)
+		if err == nil {
+			break
+		}
+	}
+	if db == nil {
+		return false, err
+	}
+	defer db.Close()
+	db.SetConnMaxLifetime(5 * time.Second)
+	db.SetMaxIdleConns(0)
+	err = db.Ping()
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
