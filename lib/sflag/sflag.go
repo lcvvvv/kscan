11883ebbeacc712ac11315314b8bb46d6b49cc17
kv2 <cheng.kv2@gmail.com>
2022-07-26 16:41:04 +0800

    [+]新增StringSplice、IntSplice参数类型，会自动对输入参数序列化成切片

--- lib/sflag/sflag.go
+++ lib/sflag/sflag.go
@@ -3,17 +3,21 @@ package sflag
 import (
 	"flag"
 	"fmt"
+	"io"
 	"kscan/lib/misc"
 	"os"
 	"regexp"
+	"strconv"
+	"strings"
 )
 
 var (
-	autoStringSlice []string
-	multipleIntRegx = regexp.MustCompile("^((?:[0-9]+)(?:-[0-9]+)?)(?:,(?:[0-9]+)(?:-[0-9]+)?)*$")
-	multipleStrRegx = regexp.MustCompile("^([.A-Za-z0-9/]+)(,[.A-Za-z0-9/])*$")
-	proxyStrRegx    = regexp.MustCompile("^(http|https|socks5|socks4)://[0-9.]+:[0-9]+$")
-	NetlocRegx      = regexp.MustCompile("^([.A-Za-z0-9-]+):\\d+$")
+	autoStringSlice      []string
+	stringSpliceParamMap = make(map[*string]*[]string)
+	intSpliceParamMap    = make(map[*string]*[]int)
+	intRangeRegx         = regexp.MustCompile("^([0-9]+)-([0-9]+)$")
+	proxyStrRegx         = regexp.MustCompile("^(http|https|socks5|socks4)://[0-9.]+:[0-9]+$")
+	NetlocRegx           = regexp.MustCompile("^([.A-Za-z0-9-]+):\\d+$")
 )
 
 func BoolVar(p *bool, name string, value bool) {
@@ -26,6 +30,30 @@ func StringVar(p *string, name string, value string) {
 
 func IntVar(p *int, name string, value int) {
 	flag.IntVar(p, name, value, "")
+}
+
+func StringSpliceVar(p *[]string, name string) {
+	for keyPtr, ptr := range stringSpliceParamMap {
+		if ptr == p {
+			flag.StringVar(keyPtr, name, "", "")
+			return
+		}
+	}
+	var s string
+	flag.StringVar(&s, name, "", "")
+	stringSpliceParamMap[&s] = p
+}
+
+func IntSpliceVar(p *[]int, name string) {
+	for keyPtr, ptr := range intSpliceParamMap {
+		if ptr == p {
+			flag.StringVar(keyPtr, name, "", "")
+			return
+		}
+	}
+	var s string
+	flag.StringVar(&s, name, "", "")
+	intSpliceParamMap[&s] = p
 }
 
 func AutoVarString(p *string, name string, value string) {
@@ -42,15 +70,68 @@ func SetUsage(s string) {
 func Parse() {
 	fixArgs()
 	flag.Parse()
+	for strPtr, splicePtr := range stringSpliceParamMap {
+		*splicePtr = parseStringSpliceExpress(*strPtr)
+	}
+	for intPtr, splicePtr := range intSpliceParamMap {
+		*splicePtr = parseIntSpliceExpress(*intPtr)
+	}
 }
 
-func MultipleIntVerification(s string) bool {
-	return multipleIntRegx.MatchString(s)
+func parseStringSpliceExpress(expr string) (splice []string) {
+	if expr == "" {
+		return splice
+	}
+	//判断对象是否为多个
+	if s := strings.ReplaceAll(expr, "\\,", "[DouHao]"); strings.Count(s, ",") > 0 {
+		for _, str := range strings.Split(s, ",") {
+			splice = append(splice, strings.ReplaceAll(str, "[DouHao]", ","))
+		}
+		return splice
+	}
+	//判断target字符串是否为文件
+	if regexp.MustCompile("^file:.+$").MatchString(expr) {
+		expr = strings.Replace(expr, "file:", "", 1)
+	}
+	if _, err := os.Lstat(expr); os.IsNotExist(err) == false {
+		fs, err := os.Open(expr)
+		if err != nil {
+			panic(err)
+		}
+		buf, err := io.ReadAll(fs)
+		if err != nil {
+			panic(err)
+		}
+		splice = strings.Split(string(buf), "\n")
+		for i, s := range splice {
+			splice[i] = strings.TrimSpace(s)
+		}
+		return splice
+	}
+	return append(splice, expr)
 }
 
-func MultipleStrVerification(s string) bool {
-	return multipleStrRegx.MatchString(s)
-
+func parseIntSpliceExpress(expr string) (splice []int) {
+	for _, sv := range parseStringSpliceExpress(expr) {
+		iv, err := strconv.Atoi(sv)
+		if err == nil {
+			splice = append(splice, iv)
+			continue
+		}
+		if intRangeRegx.MatchString(sv) == true {
+			regRes := intRangeRegx.FindStringSubmatch(sv)
+			start, _ := strconv.Atoi(regRes[1])
+			end, _ := strconv.Atoi(regRes[2])
+			if end > start {
+				for j := start; j <= end; j++ {
+					splice = append(splice, j)
+				}
+				continue
+			}
+		}
+		panic(err)
+	}
+	return splice
 }
 
 func NetlocVerification(s string) bool {
@@ -59,7 +140,6 @@ func NetlocVerification(s string) bool {
 
 func ProxyStrVerification(s string) bool {
 	return proxyStrRegx.MatchString(s)
-
 }
 
 func fixArgs() {
